bad15a3f66150e30925e9847283f67fd17470e3e
Trent Keyes <tkeyes@gmail.com>
2022-07-19 21:53:04 -0500

    Separate functions and begin organizing returned weather data

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,120 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/date-fns/esm/_lib/requiredArgs/index.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/date-fns/esm/_lib/requiredArgs/index.js ***!
+  \**************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ requiredArgs)\n/* harmony export */ });\nfunction requiredArgs(required, args) {\n  if (args.length < required) {\n    throw new TypeError(required + ' argument' + (required > 1 ? 's' : '') + ' required, but only ' + args.length + ' present');\n  }\n}\n\n//# sourceURL=webpack://weather-app/./node_modules/date-fns/esm/_lib/requiredArgs/index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/date-fns/esm/_lib/toInteger/index.js":
+/*!***********************************************************!*\
+  !*** ./node_modules/date-fns/esm/_lib/toInteger/index.js ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ toInteger)\n/* harmony export */ });\nfunction toInteger(dirtyNumber) {\n  if (dirtyNumber === null || dirtyNumber === true || dirtyNumber === false) {\n    return NaN;\n  }\n\n  var number = Number(dirtyNumber);\n\n  if (isNaN(number)) {\n    return number;\n  }\n\n  return number < 0 ? Math.ceil(number) : Math.floor(number);\n}\n\n//# sourceURL=webpack://weather-app/./node_modules/date-fns/esm/_lib/toInteger/index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/date-fns/esm/fromUnixTime/index.js":
+/*!*********************************************************!*\
+  !*** ./node_modules/date-fns/esm/fromUnixTime/index.js ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ fromUnixTime)\n/* harmony export */ });\n/* harmony import */ var _toDate_index_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../toDate/index.js */ \"./node_modules/date-fns/esm/toDate/index.js\");\n/* harmony import */ var _lib_toInteger_index_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../_lib/toInteger/index.js */ \"./node_modules/date-fns/esm/_lib/toInteger/index.js\");\n/* harmony import */ var _lib_requiredArgs_index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../_lib/requiredArgs/index.js */ \"./node_modules/date-fns/esm/_lib/requiredArgs/index.js\");\n\n\n\n/**\n * @name fromUnixTime\n * @category Timestamp Helpers\n * @summary Create a date from a Unix timestamp.\n *\n * @description\n * Create a date from a Unix timestamp (in seconds). Decimal values will be discarded.\n *\n * ### v2.0.0 breaking changes:\n *\n * - [Changes that are common for the whole library](https://github.com/date-fns/date-fns/blob/master/docs/upgradeGuide.md#Common-Changes).\n *\n * @param {Number} unixTime - the given Unix timestamp (in seconds)\n * @returns {Date} the date\n * @throws {TypeError} 1 argument required\n *\n * @example\n * // Create the date 29 February 2012 11:45:05:\n * const result = fromUnixTime(1330515905)\n * //=> Wed Feb 29 2012 11:45:05\n */\n\nfunction fromUnixTime(dirtyUnixTime) {\n  (0,_lib_requiredArgs_index_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(1, arguments);\n  var unixTime = (0,_lib_toInteger_index_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(dirtyUnixTime);\n  return (0,_toDate_index_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(unixTime * 1000);\n}\n\n//# sourceURL=webpack://weather-app/./node_modules/date-fns/esm/fromUnixTime/index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/date-fns/esm/toDate/index.js":
+/*!***************************************************!*\
+  !*** ./node_modules/date-fns/esm/toDate/index.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ toDate)\n/* harmony export */ });\n/* harmony import */ var _lib_requiredArgs_index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../_lib/requiredArgs/index.js */ \"./node_modules/date-fns/esm/_lib/requiredArgs/index.js\");\n\n/**\n * @name toDate\n * @category Common Helpers\n * @summary Convert the given argument to an instance of Date.\n *\n * @description\n * Convert the given argument to an instance of Date.\n *\n * If the argument is an instance of Date, the function returns its clone.\n *\n * If the argument is a number, it is treated as a timestamp.\n *\n * If the argument is none of the above, the function returns Invalid Date.\n *\n * **Note**: *all* Date arguments passed to any *date-fns* function is processed by `toDate`.\n *\n * @param {Date|Number} argument - the value to convert\n * @returns {Date} the parsed date in the local time zone\n * @throws {TypeError} 1 argument required\n *\n * @example\n * // Clone the date:\n * const result = toDate(new Date(2014, 1, 11, 11, 30, 30))\n * //=> Tue Feb 11 2014 11:30:30\n *\n * @example\n * // Convert the timestamp to date:\n * const result = toDate(1392098430000)\n * //=> Tue Feb 11 2014 11:30:30\n */\n\nfunction toDate(argument) {\n  (0,_lib_requiredArgs_index_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(1, arguments);\n  var argStr = Object.prototype.toString.call(argument); // Clone the date\n\n  if (argument instanceof Date || typeof argument === 'object' && argStr === '[object Date]') {\n    // Prevent the date to lose the milliseconds when passed to new Date() in IE10\n    return new Date(argument.getTime());\n  } else if (typeof argument === 'number' || argStr === '[object Number]') {\n    return new Date(argument);\n  } else {\n    if ((typeof argument === 'string' || argStr === '[object String]') && typeof console !== 'undefined') {\n      // eslint-disable-next-line no-console\n      console.warn(\"Starting with v2.0.0-beta.1 date-fns doesn't accept strings as date arguments. Please use `parseISO` to parse strings. See: https://git.io/fjule\"); // eslint-disable-next-line no-console\n\n      console.warn(new Error().stack);\n    }\n\n    return new Date(NaN);\n  }\n}\n\n//# sourceURL=webpack://weather-app/./node_modules/date-fns/esm/toDate/index.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("const searchInput = document.getElementById('search');\nconst searchBtn = document.getElementById('searchBtn');\n\nasync function getWeatherData() {\n  const input = searchInput.value;\n  // get lat and lon from city entered\n  const latLonResponse = await fetch(\n    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=5a5e3111582419d8b091603e36061930`,\n    { mode: 'cors' }\n  );\n  const latLonData = await latLonResponse.json();\n  const [lat, lon] = [latLonData[0].lat, latLonData[0].lon];\n  // use lat and lon to find current city weather data (imperial units)\n  const weatherResponse = await fetch(\n    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,\n    { mode: 'cors' }\n  );\n  const weatherData = await weatherResponse.json();\n  // find city forecast (imperial units)\n  const forecastResponse = await fetch(\n    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,\n    { mode: 'cors' }\n  );\n  const forecastData = await forecastResponse.json();\n\n  console.log(weatherData);\n  console.log(forecastData);\n}\n\nsearchBtn.addEventListener('click', getWeatherData);\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var date_fns__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! date-fns */ \"./node_modules/date-fns/esm/fromUnixTime/index.js\");\n\n\nconst searchInput = document.getElementById('search');\nconst searchBtn = document.getElementById('searchBtn');\n\nconst getLatLon = async (input) => {\n  const latLonResponse = await fetch(\n    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=5a5e3111582419d8b091603e36061930`,\n    { mode: 'cors' }\n  );\n  const latLonData = await latLonResponse.json();\n  const [lat, lon] = [latLonData[0].lat, latLonData[0].lon];\n  return [lat, lon];\n};\n\nconst getCurrentWeather = async (location) => {\n  const [lat, lon] = location;\n  const weatherResponse = await fetch(\n    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,\n    { mode: 'cors' }\n  );\n  const weatherData = await weatherResponse.json();\n  return weatherData;\n};\n\nconst getForecastWeather = async (location) => {\n  const [lat, lon] = location;\n  const forecastResponse = await fetch(\n    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,\n    { mode: 'cors' }\n  );\n  const forecastData = await forecastResponse.json();\n  return forecastData;\n};\n\nconst processWeatherData = async () => {\n  const input = searchInput.value;\n  const [lat, lon] = await getLatLon(input);\n  const [currentWeatherData, forecastWeatherData] = await Promise.all([\n    getCurrentWeather([lat, lon]),\n    getForecastWeather([lat, lon]),\n  ]).then((data) => data);\n\n  console.log(currentWeatherData);\n  console.log(forecastWeatherData);\n\n  const currentWeather = {\n    main: currentWeatherData.weather[0].main,\n    description: currentWeatherData.weather[0].description,\n    temp: currentWeatherData.main.temp,\n    tempMin: currentWeatherData.main.temp_min,\n    tempMax: currentWeatherData.main.temp_max,\n    feelsLike: currentWeatherData.main.feels_like,\n    humidity: currentWeatherData.main.humidity,\n    windSpeed: currentWeatherData.wind.speed,\n    time: (0,date_fns__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(currentWeatherData.dt),\n  };\n  console.log(currentWeather);\n  console.log(`Feels like ${currentWeather.feelsLike}`);\n  console.log(`Time: ${currentWeather.time}`);\n\n  const forecastWeather = {};\n\n  return [currentWeather, forecastWeather];\n};\n\nsearchBtn.addEventListener('click', processWeatherData);\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,67 @@
+import { fromUnixTime } from 'date-fns';
+
 const searchInput = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
 
-async function getWeatherData() {
-  const input = searchInput.value;
-  // get lat and lon from city entered
+const getLatLon = async (input) => {
   const latLonResponse = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=5a5e3111582419d8b091603e36061930`,
     { mode: 'cors' }
   );
   const latLonData = await latLonResponse.json();
   const [lat, lon] = [latLonData[0].lat, latLonData[0].lon];
-  // use lat and lon to find current city weather data (imperial units)
+  return [lat, lon];
+};
+
+const getCurrentWeather = async (location) => {
+  const [lat, lon] = location;
   const weatherResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,
     { mode: 'cors' }
   );
   const weatherData = await weatherResponse.json();
-  // find city forecast (imperial units)
+  return weatherData;
+};
+
+const getForecastWeather = async (location) => {
+  const [lat, lon] = location;
   const forecastResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5a5e3111582419d8b091603e36061930&units=metric`,
     { mode: 'cors' }
   );
   const forecastData = await forecastResponse.json();
+  return forecastData;
+};
 
-  console.log(weatherData);
-  console.log(forecastData);
-}
+const processWeatherData = async () => {
+  const input = searchInput.value;
+  const [lat, lon] = await getLatLon(input);
+  const [currentWeatherData, forecastWeatherData] = await Promise.all([
+    getCurrentWeather([lat, lon]),
+    getForecastWeather([lat, lon]),
+  ]).then((data) => data);
 
-searchBtn.addEventListener('click', getWeatherData);
+  console.log(currentWeatherData);
+  console.log(forecastWeatherData);
+
+  const currentWeather = {
+    main: currentWeatherData.weather[0].main,
+    description: currentWeatherData.weather[0].description,
+    temp: currentWeatherData.main.temp,
+    tempMin: currentWeatherData.main.temp_min,
+    tempMax: currentWeatherData.main.temp_max,
+    feelsLike: currentWeatherData.main.feels_like,
+    humidity: currentWeatherData.main.humidity,
+    windSpeed: currentWeatherData.wind.speed,
+    time: fromUnixTime(currentWeatherData.dt),
+  };
+  console.log(currentWeather);
+  console.log(`Feels like ${currentWeather.feelsLike}`);
+  console.log(`Time: ${currentWeather.time}`);
+
+  const forecastWeather = {};
+
+  return [currentWeather, forecastWeather];
+};
+
+searchBtn.addEventListener('click', processWeatherData);
