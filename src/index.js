b4c8fe5c75db1253c7a3df1c1e887325d204adc2
Trent Keyes <tkeyes@gmail.com>
2022-07-20 20:26:55 -0500

    Display current data, add styling, toggle switch between units

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { fromUnixTime } from 'date-fns';
 
 const searchInput = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
+const unitToggle = document.getElementById('unitToggle');
+
+let unitSystem = 'metric';
+let activeCity = 'Lisbon';
 
 const getLatLon = async (input) => {
   const latLonResponse = await fetch(
@@ -17,7 +21,7 @@ const getLatLon = async (input) => {
 const getWeather = async (location) => {
   const [lat, lon] = location;
   const weatherResponse = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=5a5e3111582419d8b091603e36061930&units=metric`,
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=5a5e3111582419d8b091603e36061930&units=${unitSystem}`,
     { mode: 'cors' }
   );
   const weatherData = await weatherResponse.json();
@@ -27,8 +31,7 @@ const getWeather = async (location) => {
 const adjustedForTimezone = (shiftedUnixTime) =>
   fromUnixTime(shiftedUnixTime).toUTCString().replace(' GMT', '');
 
-const processWeatherData = async () => {
-  const city = searchInput.value;
+const processWeatherData = async (city) => {
   const [lat, lon] = await getLatLon(city);
   const weatherData = await getWeather([lat, lon]);
 
@@ -49,15 +52,72 @@ const processWeatherData = async () => {
     tempMax: weatherData.daily[0].temp.max,
     dailyForecasts: weatherData.daily,
   };
+  //format and split up date and time
+  // add city
+  //icon
+  //round decimals with Math.round(0.9)
 
   console.log(weather);
   console.log(`Local time: ${weather.time}`);
-
   return weather;
 };
 
-// const renderWeatherData = () => {
-//   //
-// };
+const addWeatherData = (data) => {
+  let tempUnit;
+  let speedUnit;
+  if (unitSystem === 'metric') {
+    tempUnit = 'C';
+    speedUnit = 'km/h';
+  } else {
+    tempUnit = 'F';
+    speedUnit = 'mp/h';
+  }
 
-searchBtn.addEventListener('click', processWeatherData);
+  const temp = document.getElementById('temp');
+  const desc = document.getElementById('desc');
+  const city = document.getElementById('city');
+  const date = document.getElementById('date');
+  const time = document.getElementById('time');
+  const feelsLike = document.getElementById('feelsLike');
+  const humidity = document.getElementById('humidity');
+  const windSpeed = document.getElementById('windSpeed');
+
+  temp.textContent = `${data.temp} °${tempUnit}`;
+  desc.textContent = data.description;
+  //city.textContent = data.city
+  date.textContent = data.time;
+  time.textContent = data.time;
+  feelsLike.textContent = `Feels Like: ${data.feelsLike} °${tempUnit}`;
+  humidity.textContent = `Humidity: ${data.humidity}`;
+  windSpeed.textContent = `Wind Speed: ${data.windSpeed} ${speedUnit}`;
+  console.log('test');
+  console.log(data);
+};
+
+const displayWeatherData = async (city) => {
+  const data = await processWeatherData(city);
+  addWeatherData(data);
+};
+
+const inputWeatherData = async () => {
+  const city = searchInput.value;
+  activeCity = city;
+  searchInput.value = '';
+  displayWeatherData(city);
+};
+
+const toggleUnits = () => {
+  if (unitSystem === 'metric') {
+    unitSystem = 'imperial';
+    unitToggle.textContent = 'Display °C';
+  } else {
+    unitSystem = 'metric';
+    unitToggle.textContent = 'Display °F';
+  }
+  displayWeatherData(activeCity);
+};
+
+searchBtn.addEventListener('click', inputWeatherData);
+unitToggle.addEventListener('click', toggleUnits);
+
+displayWeatherData('Lisbon');
